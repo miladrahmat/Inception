c038b0acaee39e7a0d4eb91aa90aa61a14d42c63
Milad <milad.rahmat@gmail.com>
2025-09-02 18:34:55 +0300

    Got the database to work and nginx

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql" ]; then
-	mariadb-install-db --basedir=/usr --user=mysqluser --datadir=/var/lib/mysql
+# if [ ! -d "/var/lib/mysql" ]; then
+	mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql
+# fi
+echo "HERE"
+mysqld --user=mysql --bootstrap << EOF
+	USE mysql;
+	FLUSH PRIVLIGES;
+	ALTER USER 'root'@'localhost' IDENTIFIED BY "DataBasePassword";
+	CREATE DATABASE MyData;
+	CREATE USER "user"@'%' IDENTIFIED BY "WordPressDBPassword";
+	GRANT ALL PRIVLEGES ON MyData.* TO '"user"'@'%';
+	FLUSH PRIVLEGES;
+EOF
 
-	mysqld --user=mysqluser --bootstrap << EOF
-		USE mysql;
-		FLUSH PRIVLIGES;
-		ALTER USER 'root'@'localhost' IDENTIFIED BY "DataBasePassword";
-		CREATE DATABASE "MyData";
-		CREATE USER "MySQLUser"@'%' IDENTIFIED BY "WordPressDBPassword";
-		GRANT ALL PRIVLEGES ON "MyData".* TO '"MySQLUser"'@'%';
-		FLUSH PRIVLEGES;
-		EOF
-fi
-
-exec mysqld --defaults-file=/etc/my.cnf.d/mariadb_config
+exec mysqld --user=mysql
