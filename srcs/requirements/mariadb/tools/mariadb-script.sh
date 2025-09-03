46d2c167b228e8f5150e1f964aa003a3ae4260d0
Milad <milad.rahmat@gmail.com>
2025-09-03 17:12:57 +0300

    Fixed typos, started working on wordpress

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -5,14 +5,14 @@
 # fi
 root_pw=$(cat /run/secrets/db_root_password.txt)
 user_pw=$(cat /run/secrets/db_password.txt)
-mysqld --user=mariadb --bootstrap << EOF
+mysqld --user=mariadb --bootstrap <<- EOF
 	USE mysql;
-	FLUSH PRIVLIGES;
+	FLUSH PRIVILEGES;
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '$root_pw';
 	CREATE DATABASE $DB_NAME;
 	CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$user_pw';
 	GRANT ALL PRIVLEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-	FLUSH PRIVLEGES;
+	FLUSH PRIVILEGES;
 EOF
 
 exec mysqld --user=mariadb
